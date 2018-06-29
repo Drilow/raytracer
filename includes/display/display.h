e9358c585c9c262a63c432fe4944233e28ebebec
Alexis LACROISADE <alacrois@e2r10p14.42.fr>
2018-06-29 17:06:27 +0200

    antialiasing...

--- includes/display/display.h
+++ includes/display/display.h
@@ -6,12 +6,13 @@
 /*   By: Dagnear <Dagnear@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/22 16:06:31 by adleau            #+#    #+#             */
-/*   Updated: 2018/06/22 23:10:36 by alacrois         ###   ########.fr       */
+/*   Updated: 2018/06/29 16:07:01 by alacrois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DISPLAY_H
 # define DISPLAY_H
+# include <sdl_stuff/sdl_mgr.h>
 
 void			draw_image(t_sdl_wrapper *e);
 void			antialiasing(t_sdl_wrapper *e);
