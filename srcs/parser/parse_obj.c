316594aa073eb8494c0b8ee604331c716b4e5ed2
mabessir <mabessir@student.42.fr>
2018-10-29 17:45:49 +0100

    Started obj parse.

---
+++ srcs/parser/parse_obj.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_obj.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabessir <mabessir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/29 17:13:15 by mabessir          #+#    #+#             */
+/*   Updated: 2018/10/29 17:13:28 by mabessir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <global.h>
+#include <libft.h>
+#include <parser/parser.h>
+#include <extra/extra_defs.h>
+#include <objects/object.h>
+#include <fcntl.h>
+
