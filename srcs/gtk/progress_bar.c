b0eab88a5e53168552a5d03d870ef0b46daf34ad
Clement PAYS <cpays@e2r13p12.42.fr>
2018-11-30 18:57:00 +0100

    fix & norme

--- srcs/gtk/progress_bar.c
+++ srcs/gtk/progress_bar.c
@@ -9,102 +9,102 @@
 /*   Updated: 2018/11/15 20:05:25 by Dagnear          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <libft.h>
 #include <rt.h>
 #include <thread/thread.h>
 #include <global.h>
 #define PROGRESS_DATA g_global.r->gtk_mgr.ui.progress_data
 
-extern t_global            g_global;
-extern enum state          state;
-extern pthread_mutex_t     mutex;
-extern pthread_cond_t      condA;
-extern pthread_cond_t      condB;
+extern t_global				g_global;
+extern enum state			g_state;
+extern pthread_mutex_t		g_mutex;
+extern pthread_cond_t		g_cond_a;
+extern pthread_cond_t		g_cond_b;
 
-gboolean updateProgress()
+gboolean		update_progress(void)
 {
-    gdouble fraction;
-    int pct;
+	gdouble		fraction;
+	int			pct;
 
-    if (PROGRESS_DATA.len > 0)
-    {
-        fraction = (gfloat)PROGRESS_DATA.pos / (gfloat)PROGRESS_DATA.len;
-        pct = fraction * 100;
-        if (PROGRESS_DATA.nLastPct != pct)
-        {
-            gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(PROGRESS_DATA.pbar),
-                               fraction);
-            gtk_progress_bar_set_text(GTK_PROGRESS_BAR(PROGRESS_DATA.pbar),
-            	ft_strjoin(ft_itoa(pct), " %"));
-             while (gtk_events_pending())
-                 gtk_main_iteration();
-            PROGRESS_DATA.nLastPct = pct;
-        }
-    }
-    if (PROGRESS_DATA.nLastPct >= 100)
-    	return(TRUE);
-    return FALSE;
+	if (PROGRESS_DATA.len > 0)
+	{
+		fraction = (gfloat)PROGRESS_DATA.pos / (gfloat)PROGRESS_DATA.len;
+		pct = fraction * 100;
+		if (PROGRESS_DATA.nLastPct != pct)
+		{
+			gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(PROGRESS_DATA.pbar),
+							fraction);
+			gtk_progress_bar_set_text(GTK_PROGRESS_BAR(PROGRESS_DATA.pbar),
+				ft_strjoin(ft_itoa(pct), " %"));
+			while (gtk_events_pending())
+				gtk_main_iteration();
+			PROGRESS_DATA.nLastPct = pct;
+		}
+	}
+	if (PROGRESS_DATA.nLastPct >= 100)
+		return (TRUE);
+	return (FALSE);
 }
 
-static void EndProgress ()
+static void		end_progress(void)
 {
-    PROGRESS_DATA.bProgressUp = FALSE;
-    gtk_widget_destroy(PROGRESS_DATA.window);
+	PROGRESS_DATA.bProgressUp = FALSE;
+	gtk_widget_destroy(PROGRESS_DATA.window);
 }
 
-void			progress_bar()
+void			progress_bar(void)
 {
-    PROGRESS_DATA.nLastPct = -1;
-    PROGRESS_DATA.bProgressUp = TRUE;
-    PROGRESS_DATA.pbar = NULL;
-    PROGRESS_DATA.window = NULL;
-    PROGRESS_DATA.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(PROGRESS_DATA.window), "Loading");
+	PROGRESS_DATA.nLastPct = -1;
+	PROGRESS_DATA.bProgressUp = TRUE;
+	PROGRESS_DATA.pbar = NULL;
+	PROGRESS_DATA.window = NULL;
+	PROGRESS_DATA.window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(PROGRESS_DATA.window), "Loading");
 	gtk_window_set_position(GTK_WINDOW(PROGRESS_DATA.window),
 		GTK_WIN_POS_CENTER);
 	gtk_window_set_default_size(GTK_WINDOW(PROGRESS_DATA.window), 220, 20);
-    PROGRESS_DATA.pbar = gtk_progress_bar_new();
-    gtk_progress_bar_set_show_text(GTK_PROGRESS_BAR(PROGRESS_DATA.pbar), TRUE);
-    gtk_container_add(GTK_CONTAINER(PROGRESS_DATA.window), PROGRESS_DATA.pbar);
-
-    gtk_widget_show_all(PROGRESS_DATA.window);
+	PROGRESS_DATA.pbar = gtk_progress_bar_new();
+	gtk_progress_bar_set_show_text(GTK_PROGRESS_BAR(PROGRESS_DATA.pbar), TRUE);
+	gtk_container_add(GTK_CONTAINER(PROGRESS_DATA.window), PROGRESS_DATA.pbar);
+	gtk_widget_show_all(PROGRESS_DATA.window);
 }
 
-void		progress_thread_handler(gdouble x)
+void			progress_thread_handler(gdouble x)
 {
-	pthread_mutex_lock(&mutex);
-    while (state != STATE_B)
-        pthread_cond_wait(&condB, &mutex);
-    pthread_mutex_unlock(&mutex);
+	pthread_mutex_lock(&g_mutex);
+	while (g_state != STATE_B)
+		pthread_cond_wait(&g_cond_b, &g_mutex);
+	pthread_mutex_unlock(&g_mutex);
 	PROGRESS_DATA.pos = x;
 	PROGRESS_DATA.len = WIN_W;
-	pthread_mutex_lock(&mutex);
-    state = STATE_A;
-    pthread_cond_signal(&condA);
-    pthread_mutex_unlock(&mutex);
+	pthread_mutex_lock(&g_mutex);
+	g_state = STATE_A;
+	pthread_cond_signal(&g_cond_a);
+	pthread_mutex_unlock(&g_mutex);
 }
 
-void		progress_main_handler()
+void			progress_main_handler(void)
 {
-	bool bGotSomeData;
+	bool		b_lock;
 
-	bGotSomeData = FALSE;
-	while (bGotSomeData == FALSE)
-    {
-    	pthread_mutex_lock(&mutex);
-        while (state != STATE_A)
-            pthread_cond_wait(&condA, &mutex);
-        pthread_mutex_unlock(&mutex);
-        bGotSomeData = updateProgress();
-        if(bGotSomeData == FALSE)
-        {
-        	pthread_mutex_lock(&mutex);
-    	    while (gtk_events_pending())
-                 gtk_main_iteration();
-             state = STATE_B;
-    	    pthread_cond_signal(&condB);
-	        pthread_mutex_unlock(&mutex);
-        }
-    }
-    EndProgress();
+	b_lock = FALSE;
+	while (b_lock == FALSE)
+	{
+		pthread_mutex_lock(&g_mutex);
+		while (g_state != STATE_A)
+			pthread_cond_wait(&g_cond_a, &g_mutex);
+		pthread_mutex_unlock(&g_mutex);
+		b_lock = update_progress();
+		if (b_lock == FALSE)
+		{
+			pthread_mutex_lock(&g_mutex);
+			while (gtk_events_pending())
+				gtk_main_iteration();
+			g_state = STATE_B;
+			pthread_cond_signal(&g_cond_b);
+			pthread_mutex_unlock(&g_mutex);
+		}
+	}
+	end_progress();
 }
--- srcs/gtk/scene_view.c
+++ srcs/gtk/scene_view.c
@@ -21,21 +21,20 @@
 
 extern t_global		g_global;
 
-void 				append_column_with_text(GtkWidget *tree, char *text,
-									GtkCellRenderer *renderer, int en_column)
+void					append_column_with_text(GtkWidget *tree, char *text,
+	GtkCellRenderer *renderer, int en_column)
 {
-	GtkTreeViewColumn *column;
+	GtkTreeViewColumn	*column;
 
 	column = gtk_tree_view_column_new_with_attributes(text, renderer,
-                                                      "text", en_column,
-                                                      NULL);
+													"text", en_column,
+													NULL);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(tree), column);
 }
 
-
-bool				go_throu_lights(t_light *curr)
+bool					go_throu_lights(t_light *curr)
 {
-	t_light	*tmp;
+	t_light				*tmp;
 
 	tmp = g_global.r->lights;
 	while (tmp)
@@ -47,78 +46,76 @@ bool				go_throu_lights(t_light *curr)
 	return (false);
 }
 
-static void 		checked_row(__attribute__((unused))GtkCellRendererToggle *cell,
-									gchar *path_str,
-									__attribute__((unused))gpointer data)
+static void				checked_row(__attribute__((unused))GtkCellRendererToggle *cell, gchar *path_str)
 {
-	GtkTreeIter  iter;
-	GtkTreePath *path;
-	gboolean enabled;
-	GtkTreeModel *model;
-	gpointer		*obj;
+	GtkTreeIter			iter;
+	GtkTreePath			*path;
+	gboolean			enabled;
+	GtkTreeModel		*model;
+	gpointer			*obj;
 
 	obj = NULL;
-	model = NULL;
-	path = gtk_tree_path_new_from_string (path_str);
-	if ((model = gtk_tree_view_get_model (GTK_TREE_VIEW (SCENE_VIEW.tree))) == NULL)
-        return ;
-	if (gtk_tree_model_get_iter (model, &iter, path) == FALSE)
+	model = gtk_tree_view_get_model(GTK_TREE_VIEW(SCENE_VIEW.tree));
+	path = gtk_tree_path_new_from_string(path_str);
+	if (model == NULL)
 		return ;
-	gtk_tree_model_get (model, &iter, CHECKED_COLUMN, &enabled, -1);
+	if (gtk_tree_model_get_iter(model, &iter, path) == FALSE)
+		return ;
+	gtk_tree_model_get(model, &iter, CHECKED_COLUMN, &enabled, -1);
 	enabled = !enabled;
 	gtk_tree_store_set(SCENE_VIEW.store, &iter, CHECKED_COLUMN, enabled, -1);
 	if (gtk_tree_model_get_iter(model, &iter, path) == FALSE)
 		return ;
-	gtk_tree_model_get (model, &iter, OBJ_REF, &obj, -1);
+	gtk_tree_model_get(model, &iter, OBJ_REF, &obj, -1);
 	if (is_obj(((t_obj*)obj)->type))
 		((t_obj*)obj)->enabled = enabled;
 	else if (go_throu_lights(((t_light*)obj)))
-			((t_light*)obj)->enabled = enabled; // a debug
+		((t_light*)obj)->enabled = enabled; // a debug
 	redraw(true);
-	gtk_tree_path_free (path);
+	gtk_tree_path_free(path);
 }
 
-void        select_handler(GtkTreeView *tree_view, GtkTreePath *path,
-						   __attribute__((unused))GtkTreeViewColumn *column,
-						   __attribute__((unused))gpointer user_data)
+void					select_handler(GtkTreeView *tree, GtkTreePath *path)
 {
-	gpointer *obj;
-	GtkTreeModel *model;
-	GtkTreeIter iter;
+	gpointer			*obj;
+	GtkTreeModel		*model;
+	GtkTreeIter			iter;
 
 	model = NULL;
-	if ((model = gtk_tree_view_get_model (tree_view)) == NULL)
+	if ((model = gtk_tree_view_get_model(tree)) == NULL)
 		return ;
 	if (gtk_tree_model_get_iter(model, &iter, path))
-    {
-		gtk_tree_model_get (model, &iter, OBJ_REF, &obj, -1);
+	{
+		gtk_tree_model_get(model, &iter, OBJ_REF, &obj, -1);
 		if (is_obj(((t_obj*)obj)->type))
 		{
-            outline_obj(((t_obj*)obj));
-            edit_win(((t_obj*)obj));
+			outline_obj(((t_obj*)obj));
+			edit_win(((t_obj*)obj));
 		}
-    }
+	}
 }
 
-void				scene_win(void)
+void					scene_win(void)
 {
-	GtkTreeViewColumn *column;
-	GtkCellRenderer *renderer;
+	GtkTreeViewColumn	*column;
+	GtkCellRenderer		*render;
 
 	init_scene_view();
-   	renderer = gtk_cell_renderer_toggle_new ();
-   	gtk_cell_renderer_toggle_set_activatable(GTK_CELL_RENDERER_TOGGLE(renderer), TRUE);
-   	g_signal_connect (G_OBJECT(renderer), "toggled", G_CALLBACK(checked_row), NULL);
-   	column = gtk_tree_view_column_new_with_attributes ("Visible",
-                                                      renderer,
-                                                      "active", CHECKED_COLUMN,
-                                                      NULL);
-   	gtk_tree_view_column_add_attribute (column, renderer, "activatable", CHECKED_ACTIVE);
+	render = gtk_cell_renderer_toggle_new();
+	gtk_cell_renderer_toggle_set_activatable(GTK_CELL_RENDERER_TOGGLE(render),
+		TRUE);
+	g_signal_connect(G_OBJECT(render), "toggled", G_CALLBACK(checked_row),
+		NULL);
+	column = gtk_tree_view_column_new_with_attributes("Visible",
+													render,
+													"active", CHECKED_COLUMN,
+													NULL);
+	gtk_tree_view_column_add_attribute(column, render, "activatable",
+		CHECKED_ACTIVE);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(SCENE_VIEW.tree), column);
-
-  	g_signal_connect(G_OBJECT(SCENE_VIEW.tree), "row-activated",
-    G_CALLBACK(select_handler), NULL);
+	g_signal_connect(G_OBJECT(SCENE_VIEW.tree), "row-activated",
+	G_CALLBACK(select_handler), NULL);
 	gtk_container_add(GTK_CONTAINER(SCENE_VIEW.win), SCENE_VIEW.tree);
-    gtk_widget_show_all(SCENE_VIEW.win);
-	return;
+	gtk_widget_show_all(SCENE_VIEW.win);
+	return ;
 }
